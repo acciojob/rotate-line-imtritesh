f816a236c2f474f2f5ac2318df61fe836573541a
imtritesh <103854328+imtritesh@users.noreply.github.com>
2023-05-23 17:45:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let div = document.querySelector("#line");
+div.style.animation = "spin 1s linear infinite"
+let style = document.createElement("style");
+style.innerHTML = `@keyframes spin{
+			0%{transform : rotate(0deg)}
+			100%{transform : rotate(360deg)}
+	   }`
+document.body.append(style);
